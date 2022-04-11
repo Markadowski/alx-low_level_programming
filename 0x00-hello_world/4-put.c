bf716603ea3a802133c1483b765f4faeb8194c59
markadowski <markadowskiosei@gmail.com>
2022-04-11 16:36:35 -0700

    task 4

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Retutn: always 0 (success)
+int main (void) 
+{ 
+Puts("/"Programming is like building a multilingual puzzle");
+return (0);
+}
