2d8eef5846f107743d7d236590e630b181876b8f
markadowski <markadowskiosei@gmail.com>
2022-04-11 17:54:58 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
 * Retutn: always 0 (success)
 int main (void) 
 { 
-Puts("/"Programming is like building a multilingual puzzle");
-return (0);
+puts("/Programming is like building a multilingual puzzle");
+return (0)
 }
