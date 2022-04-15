845d2f2a329ada33346437fb0749fb332796a72d
markadowski <markadowskiosei@gmail.com>
2022-04-14 17:26:33 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,31 @@
-#include <main.h>
+#include "main.h"
+
+
+
 /**
- * main - main
- * Return: 0
- */
+
+  * main - Test function for positive or negative
+
+  * Return: 0
+
+  */
+
 
 
 int main(void)
-[
+
+{
+
 int i;
-1 = 0;
+
+
+
+i = 0;
 
 positive_or_negative(i);
 
+
+
 return (0);
-]
+
+}
